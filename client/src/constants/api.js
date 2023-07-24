bdc4d1514f8b73590a5cdfd8a1a570a6e7ee86ee
zoharelias <zoharelias@gmail.com>
2023-07-24 21:02:31 +0300

    GET_PRODUCT_BY_ID

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,5 +1,5 @@
 export const GET_ALL_PRODUCTS_PATH = process.env.NODE_ENV === "development" ? "http://localhost:8000/" : "https://shopdemo-djxm.onrender.com/"
-export const GET_PRODUCT_BY_ID = process.env.NODE_ENV === "development" ? "http://localhost:8001/product/" : "whatever will be in the cloud"
+export const GET_PRODUCT_BY_ID = process.env.NODE_ENV === "development" ? "http://localhost:8001/product/" : "https://shopdemo-djxm.onrender.com/product"
 // export const
 // export const
 // export const
