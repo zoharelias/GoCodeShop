8beca4385b7844a1efb66bbf7cea10e8e1112628
zoharelias <zoharelias@gmail.com>
2023-07-24 21:30:30 +0300

    api fix

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,4 +1,4 @@
-export const GET_ALL_PRODUCTS_PATH = process.env.NODE_ENV === "development" ? "http://localhost:8000/api/products" : "https://shopdemo-djxm.onrender.com/api/products"
+export const GET_ALL_PRODUCTS_PATH = process.env.NODE_ENV === "development" ? "http://localhost:8000/api/" : "https://shopdemo-djxm.onrender.com/api"
 export const GET_PRODUCT_BY_ID = process.env.NODE_ENV === "development" ? "http://localhost:8001/api/product/" : "https://shopdemo-djxm.onrender.com/api/product"
 // export const
 // export const
